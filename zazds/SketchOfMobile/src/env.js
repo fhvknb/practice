002ab120652f1124fae80f4835d8a06a3c60b4ff
xiang510 <vip_319@126.com>
2021-02-26 23:40:08 +0800

    remove

--- zazds/SketchOfMobile/src/env.js
+++
@@ -1,3 +0,0 @@
-export const mode = 'dev'; // pro　生产　dev 开发
-export const version = 10003;
-export const baseHost = 'xxx'
--- zazds/TrainingForTS/src/calc.ts
+++
@@ -1,5 +0,0 @@
-export default  function sum(...vals: number[]): number {
-    return vals.reduce( (total, val) => {
-        return total += val
-    });
-}   
