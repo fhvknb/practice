d28ba50a09a0bc5cec416e141570f4a89f303233
wb_xiangchao <wb_xiangchao@localhost>
2021-03-08 18:57:24 +0800

    update

---
+++ LeetCode/fib/main5.py
@@ -0,0 +1,36 @@
+# 动态规划解
+
+
+# def fib(n):
+#     if n == 0:
+#         return 0
+#     if n == 1 or n == 2:
+#         return 1
+
+#     pre = 1
+#     cur = 1
+#     for i in range(n+1):
+#         if i > 2:
+#             sum = pre + cur
+#             pre = cur
+#             cur = sum
+#     return cur
+
+
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1 or n == 2:
+        return 1
+    pre = 1
+    cur = 1
+
+    for i in range(n):
+        if i > 1:
+            sum = pre + cur
+            cur = sum
+            pre = cur
+    
+    return cur
+
+print(fib(3))
