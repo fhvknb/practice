3a3e40812d21f9187b4e0f3803eeec7d97f8cbfd
wb_xiangchao <wb_xiangchao@localhost>
2021-03-18 14:55:42 +0800

    update

--- zazds/TrainingForTS/src/index.ts
+++ zazds/TrainingForTS/src/index.ts
@@ -3,7 +3,8 @@ import calc from './calc';
 let printMessage = (msg: string):void  =>  console.log(`Message: ${msg}.`);
 
 console.clear();
-console.log('>>>>>');
+
+
 console.log('hello world!');
 printMessage('This is a message!');
 printMessage('Today is a wonderful day!');
@@ -14,3 +15,13 @@ data.set("Bob", 'ShenZhen');
 data.forEach( (val, key) => console.log(`${key} lives ${val}.`));
 let calcResult = calc(1,2,3,4,5,6,7,8,9);
 console.log('Result: ' + calcResult);
+
+interface Cat {
+    [propName: string]: any
+}
+
+let newCat:Cat = {
+    name: 'x',
+    age: 12,
+    height: 1000
+}
