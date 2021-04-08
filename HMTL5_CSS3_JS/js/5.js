a7987b213be8402e01960dc0c88f49b3d3ae0a8a
wb_xiangchao <wb_xiangchao@localhost>
2021-04-08 20:01:27 +0800

    update

--- HMTL5_CSS3_JS/js/5.js
+++ HMTL5_CSS3_JS/js/5.js
@@ -42,4 +42,41 @@ var plus = curry(function () {
   return result;
 }, 2);
 
-console.log(plus(3)()()(3));
+// console.log(plus(3)()()(3));
+
+var scareMe = function () {
+  alert("Boo!");
+  scareMe = function () {
+    alert("Double boo!");
+  };
+};
+
+// 1. 添加新属性
+scareMe.property = "properly";
+// 2. scareMe赋与一个新值
+var prank = scareMe;
+// 3. 作为一个方法调用
+var spooky = {
+  boo: scareMe,
+};
+
+
+// 使用新变量名称进行调用
+// prank(); // "Boo!"
+// prank(); // "Boo!"
+// console.log(scareMe.property); // undefined
+
+
+// console.log(prank)
+// console.log(scareMe)
+// console.log(spooky.boo)
+
+// console.log(prank.property); // "properly"
+// // 使用方法进行调用
+spooky.boo(); // "Boo!"
+spooky.boo(); // "Boo!"
+console.log(spooky.boo.property); // "properly"
+
+// scareMe(); //  boo!
+// scareMe(); // Double boo!
+// console.log(scareMe.property); // undefined
