0e7ba8e31304cdd13a58c63bd3feaa362b0fd5dd
wb_xiangchao <wb_xiangchao@localhost>
2021-05-21 15:36:48 +0800

    ts update

--- zazds/TrainingForTS/src/index.ts
+++ zazds/TrainingForTS/src/index.ts
@@ -91,7 +91,64 @@ interface NameType {
 var n1: name = 'wang';
 var n2: NameType = { name: 'wang' };
 
+/* ========================== */
+interface User {
+    name: string,
+    age: number,
+    getName: () => string,
+    getAge: () => number
+} // 如何得到 'getName' | 'getAge' ？ 
 
+
+// type GetKeyByValueType<T, Condition> = {
+//     [K in keyof T]: T[K] extends Condition ? K : never
+// }[keyof T];
+
+type GetKeyByValueType<T, Condition> = {
+    [K in keyof T]: T[K] extends Condition ? K : never
+}[keyof T];
+
+type FunctionPropNames = GetKeyByValueType<User, Function>;
+
+
+const AUser: User = {
+    name: 'xiang',
+    age: 28,
+    getName: () => 'xiang',
+    getAge: () => 28
+}
+
+const getFunction = (func: FunctionPropNames) => {
+    let res = AUser[func]();
+    console.log(res);
+}
+
+let funName: FunctionPropNames = 'getName';
+let funName2: FunctionPropNames = 'getAge';
+
+getFunction('getName')
+
+
+// 将一个map所有属性变为可选的
+type Partial1<T> = { [P in keyof T]?: T[P] }
+// 将一个map所有属性变为必选的
+type Required1<T> = { [P in keyof T]-?: T[P] }
+// 将一个map所有属性变为只读的
+type Readonly1<T> = { readonly [P in keyof T]: T[P] }
+// ts标准库未包含，将一个map所有属性变为可写的
+type Mutable<T> = { -readonly [P in keyof T]: T[P] }
+
+// 将 K 中所有的属性的值转化为 T 类型
+type myRecord<K extends keyof any, T> = { [P in K]: T };
+
+// 从 T 中取出 一系列 K 的属性
+type myPick<T, K extends keyof T> = { [P in K]: T[P] };
+
+// 如果 T 是 U 的子类型的话，那么就会T
+type myExclude<T, U> = T extends U ? never : T;
+
+// 提取出 T 包含在 U 中的元素
+type myExtract<T, U> = T extends U ? T : never;
 
 
 
