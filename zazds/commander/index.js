4009ed3bdb7373723b2dd9e3949df65fb3ddba44
wb_xiangchao <wb_xiangchao@localhost>
2021-07-13 14:38:12 +0800

    update

--- zazds/commander/index.js
+++ zazds/commander/index.js
@@ -1,16 +1,16 @@
-const commander = require('commander'); 
-const program = new commander.Command();
+#!/usr/bin/env node
+
+const commander = require('commander')
+const program = new commander.Command()
 
 program
-  .option('-d, --debug', 'output extra debugging')
-  .option('-s, --small', 'small pizza size')
-  .option('-p, --pizza-type <type>', 'flavour of pizza');
+    .option('-d, --debug', 'output extra debugging')
+    .option('-s, --small', 'small pizza size')
+    .option('-p, --pizza-type <type>', 'flavour of pizza')
 
-program.parse(process.argv);
+program.parse(process.argv)
 
-if (program.debug) console.log(program.opts());
-console.log('pizza details:');
-if (program.small) console.log('- small pizza size');
-if (program.pizzaType) console.log(`- ${program.pizzaType}`);
-
-
+if (program.debug) console.log(program.opts())
+console.log('pizza details:')
+if (program.small) console.log('- small pizza size')
+if (program.pizzaType) console.log(`- ${program.pizzaType}`)
