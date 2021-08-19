7bede96494cf10fa7df824f017d1cac3d840e561
wb_xiangchao <vip_319@126.com>
2021-08-19 17:40:52 +0800

    add vue

---
+++ zazds/VueComponents/src/components/MyStep.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="my-step-wrap">
+    <template v-for="(item, index) in taskList">
+      <div
+        class="step-item"
+        :class="{
+          'is-finished': isActive > index,
+          'is-waiting': index > isActive,
+          checked: isChecked === index,
+        }"
+        :key="index"
+        @click="isChecked = void 0"
+      >
+        <div
+          class="step-mark"
+          :class="{
+            head: index === 0,
+            last: taskList.length === index + 1,
+            inner: index > 0 && index < taskList.length - 1,
+            done: isActive > index,
+            done2: isActive - index === 1,
+          }"
+          @click="
+            (e) => {
+              handleSelect(index, e);
+            }
+          "
+        >
+          <div class="step">{{ index + 1 }}</div>
+        </div>
+        <div class="title">{{ item.name }}</div>
+        <div class="desc">{{ item.desc }}</div>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: ["taskList", "isActive"],
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      // isActive: 0, // 任务完成 索引 （0,...)
+      isChecked: void 0, // 选中任务 索引 （0,...)
+    };
+  },
+  created() {
+    console.log(this);
+  },
+  methods: {
+    handleSelect: function (index, e) {
+      // console.log(index);
+      e.preventDefault();
+      e.stopPropagation();
+      this.isChecked = index;
+    },
+  },
+};
+</script>
+
+
---
+++ zazds/VueComponents/src/pages/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h3>{{ title }}</h3>
+    <my-step :task-list="taskList" :is-active="1"></my-step>
+  </div>
+</template>
+
+<script>
+// import MyStep from "../components/MyStep.vue";
+import "../components/MyStep";
+import "../assets/g.css";
+
+export default {
+  name: "HelloWorld",
+  props: [],
+  data() {
+    return {
+      title: "这是首页",
+      taskList: [
+        {
+          name: "task1",
+          desc: "任务描述",
+        },
+        {
+          name: "task2",
+          desc: "任务描述",
+        },
+        {
+          name: "task3",
+          desc: "任务描述",
+        },
+        {
+          name: "task4",
+          desc: "任务描述",
+        },
+      ],
+    };
+  },
+
+  //   components: {
+  //     MyStep: MyStep,
+  //   },
+};
+</script>
+
+<style scoped>
+</style>
